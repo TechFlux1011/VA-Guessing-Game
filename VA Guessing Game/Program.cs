f5898866e44ec3fd2e2fd37618687ad85ba018bf
TheChilupe <thechilupe@gmail.com>
2023-05-26 10:37:21 -0400

    Add project files.

---
+++ VA Guessing Game/Program.cs
@@ -0,0 +1,34 @@
+﻿Random randomNumber = new Random();
+int magicNumber = randomNumber.Next(0, 10);
+int userNumber;
+int attemptCount = 0;
+
+do
+{
+    Console.WriteLine("Guess the Magic Number it will be between 1-10: ");
+    userNumber = Convert.ToInt32(Console.ReadLine());
+
+    attemptCount++;
+
+    if (userNumber == magicNumber)
+    {
+        Console.WriteLine("correct");
+    }
+    else if (userNumber > 10 || userNumber <= 0)
+    {
+        Console.WriteLine("That is not between 1 through 10");
+    }
+
+    else
+    {
+        Console.WriteLine("Wrong try again");
+    }
+    if (attemptCount >= 3)
+    {
+        Console.WriteLine("You've run out of attempts");
+        break;
+    }
+} while (userNumber != magicNumber);
+
+
+
